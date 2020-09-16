1691417a7a53c7550fed22aa598efd4151172b8c
diazhector98 <diazhector98@gmail.com>
2020-09-16 12:50:55 -0500

    Adding and removing weight

--- PokemonApi/index.js
+++ PokemonApi/index.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
-
+const DELETE_POKEMON_BUTTON_TEXT = "Delete"
 class Pokemon {
     constructor(data) {
         const {
@@ -14,9 +14,12 @@ class Pokemon {
     }
 }
 
+// Document Node -> Pokemon Object
+let pokemons = {
 
-pokemonCards = []
+}
 
+let totalWeight = 0
 
 const onAddPokemonButtonClicked = () => {
     const pokemonNameInputText = document.getElementById("pokemon-name-input")
@@ -33,8 +36,9 @@ const fetchPokemon = (pokemonName) => {
             console.log({response})
             pokemon = new Pokemon(response.data)
             pokemonCard = createPokemonCard(pokemon)
+            pokemons[pokemon.name] = pokemon
             addPokemonCard(pokemonCard)
-
+            addWeight(pokemon)
         })
         .catch((error) => {
             console.log({error})
@@ -48,18 +52,50 @@ const createPokemonCard = (pokemon) => {
     const container = document.createElement("div")
     const pokemonNameText = document.createElement("p")
     const pokemonWeightText = document.createElement("p")
+    const deletePokemonButton = document.createElement("button")
 
+    container.id = pokemon.name
     pokemonNameText.innerHTML = pokemon.name
     pokemonWeightText.innerHTML = pokemon.weight
+    deletePokemonButton.innerHTML = DELETE_POKEMON_BUTTON_TEXT
+
+    deletePokemonButton.addEventListener("click", deletePokemon)
 
     container.appendChild(pokemonNameText)
     container.appendChild(pokemonWeightText)
+    container.appendChild(deletePokemonButton)
 
     return container
+}
+
+const deletePokemon = (event) => {
+    const deleteButton = event.target
+    const pokemonCard = deleteButton.parentElement
+    const pokemonCardsContainer = document.getElementById("pokemon-cards")
+    const pokemonId = pokemonCard.id
+    
+    removeWeight(pokemons[pokemonId])
+    removePokemonCard(pokemonCard)
 }
 
 const addPokemonCard = (pokemonCard) => {
     const container = document.getElementById("pokemon-cards")
     container.appendChild(pokemonCard)
-    pokemonCards.push(pokemonCard)
+}
+
+const removePokemonCard = (pokemonCard) => {
+    const container = document.getElementById("pokemon-cards")
+    container.removeChild(pokemonCard)
+}
+
+const addWeight = (pokemon) => {
+    const weightTextNode = document.getElementById("total-weight-text")
+    totalWeight += pokemon.weight
+    weightTextNode.innerHTML = totalWeight.toString()
+}
+
+const removeWeight = (pokemon) => {
+    const weightTextNode = document.getElementById("total-weight-text")
+    totalWeight -= pokemon.weight
+    weightTextNode.innerHTML = totalWeight.toString()
 }
